37966ba6c87e99e7f0fbd2fd278fc6a428275153
AlexAgnoII <3alex23@gmail.com>
2018-01-22 21:09:55 +0800

    commit 2

--- scripts/exercise3.js
+++ scripts/exercise3.js
@@ -13,6 +13,16 @@ let app = new PIXI.Application({
 
 let state;
 
+let snakeLogo
+
+let SNAKE_LOGO = "images/snakelogo.png",
+    PLAY_IMG = "images/play.png",
+    LEVEL_1_IMG = "images/level1.png",
+    LEVEL_2_IMG = "images/level2.png",
+    LEVEL_3_IMG = "images/level3.png",
+    SPEED_1 = 50,
+    SPEED_2 = 125,
+    SPEED_3 = 215;
 
 PIXI.loader
     .add([])
